683ec65dcd04349ec5298a1de55b584a9f9c43f1
Mingjie-Xie <3234126808@qq.com>
2024-10-03 16:33:26 +0800

    引入图标

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,10 @@
 <script setup>
+
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+
+import '@/styles/common.scss'
 </script>
 
 <template>
