f1336018eba084e7b278013f0873ccecaf5369a0
Mingjie-Xie <3234126808@qq.com>
2024-10-03 17:37:14 +0800

    layout-吸顶导航的交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -5,9 +5,12 @@ import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 
 import '@/styles/common.scss'
+
+import LayoutFixedVue from './components/LayoutFixed.vue'
 </script>
 
 <template>
+    <LayoutFixedVue />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
