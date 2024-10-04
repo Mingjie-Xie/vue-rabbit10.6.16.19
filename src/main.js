398edc16f3d415b5c460c1521d0011d904526e8f
Mingjie-Xie <3234126808@qq.com>
2024-10-04 15:33:48 +0800

    home-图片懒加载的实现

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ import { getCategory } from '@/apis/testAPI'
 getCategory().then(res=>{
     console.log(res)
 })
+
+import { useIntersectionObserver } from '@vueuse/core'
+
+
 const app = createApp(App)
 
 
@@ -19,3 +23,22 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy',{
+    mounted(el,binding){
+        console.log(el,binding.value); 
+
+        useIntersectionObserver(
+            el,
+            ([{isIntersecting }]  )=>{
+                console.log(isIntersecting);
+
+                if(isIntersecting){
+                    //进入视口
+                    el.src = binding.value
+
+                }
+             },
+            )
+        }
+})
