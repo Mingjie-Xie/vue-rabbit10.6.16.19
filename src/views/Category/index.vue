aa436d6ff824a00832c9bd5363e65716d8f4f382
Mingjie-Xie <3234126808@qq.com>
2024-10-05 16:56:06 +0800

    home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,7 +3,7 @@
 import { getCategoryAPI } from '@/apis/category';
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router';
-
+import { getBannerAPI } from '@/apis/home'
 const categoryData = ref({})
 const route = useRoute()
 const getCategory = async () =>{
@@ -13,6 +13,17 @@ const getCategory = async () =>{
 }
 
 onMounted(() => getCategory())
+
+const bannerList = ref([])
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+})
+  console.log(res);
+  bannerList.value = res.result
+  
+}
+onMounted(() => getBanner())
 </script>
 
 <template>
@@ -25,6 +36,14 @@ onMounted(() => getCategory())
           <el-breadcrumb-item>{{categoryData.name}}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+
     </div>
   </div>
 </template>
@@ -73,6 +92,16 @@ onMounted(() => getCategory())
           }
         }
       }
+    }
+  }
+  .home-banner {
+    width: 1240px;
+    height: 500px;
+    margin: 0 auto;
+  
+    img {
+      width: 100%;
+      height: 500px;
     }
   }
 
