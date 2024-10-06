f232fdd2b5ca6fbc7a425937c99768a3287017d8
Mingjie-Xie <3234126808@qq.com>
2024-10-06 21:02:43 +0800

    二级分类-列表无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -36,6 +36,18 @@ onMounted(() =>getGoodList())
   reqData.value.page = 1
   getGoodList()
  }
+
+ const disabled = ref(false)
+ const load = async() =>{
+  console.log('加载更多数据');
+  reqData.value.page++
+  const res =await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value,...res.result.items]
+
+  if(res.result.items.length == 0){
+    disabled.value = true
+  }
+ }
 </script>
 
 <template>
@@ -55,7 +67,7 @@ onMounted(() =>getGoodList())
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
 
          <!-- <GoodsItem v-for="goods is goList" :goods="goods" :key="goods.id" /> -->
