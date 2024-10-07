62796f753694433ecc47a0160be46928fb6535cb
Mingjie-Xie <3234126808@qq.com>
2024-10-07 16:48:00 +0800

    放大镜-大图显示及其实现

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -21,8 +21,16 @@ const target = ref(null)
 const { elementX,elementY,isOutside } = useMouseInElement(target)
 const left = ref(0)
 const top = ref(0)
-watch([elementX,elementY],() => {
+
+const positionX= ref(0)
+const positionY= ref(0)
+watch([elementX,elementY,isOutside],() => {
     console.log('xy变化了');
+  
+    if(isOutside.value) return
+    console.log('后续逻辑执行了');
+    
+
     if(elementX.value>100&&elementX.value<300){
         left.value = elementX.value - 100
     }
@@ -35,6 +43,8 @@ watch([elementX,elementY],() => {
     if(elementY.value > 300){ top.value = 200 }
     if(elementY.value < 100){ top.value = 0}
     
+    positionX.value = -left.value * 2
+    positionY.value = -top.value * 2
 })
 </script>
 
@@ -46,7 +56,7 @@ watch([elementX,elementY],() => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -58,10 +68,10 @@ watch([elementX,elementY],() => {
     <div class="large" :style="[
       {
         backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
