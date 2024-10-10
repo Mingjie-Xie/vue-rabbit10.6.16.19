49aedf3c8dbab52c16b477ef12254bb824847dbd
Mingjie-Xie <3234126808@qq.com>
2024-10-10 09:21:26 +0800

    登录-token生效401拦截

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import {useUserStore} from '@/stores/user'
-
+import router from '../router';
 const httpInstance = axios.create({
     baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000
@@ -25,12 +25,19 @@ httpInstance.interceptors.request.use(config => {
   
   // axios响应式拦截器
  httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   //统一错误提示
   ElMessage({
     type: 'warning', 
     message: e.response.data.message
     
   })
+
+  if(e.response.status == 401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
+
 
     return Promise.reject(e)
   })
